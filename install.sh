353f56c9a51c334f25f686b453bd9d41fcaa56a6
Invader788 <skorikandrew7@proton.me>
2026-04-12 07:33:25 -0400

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+sudo pacman -S harfbuzz
+sudo pacman -S libxinerama
+sudo pacman -S libxft
+sudo pacman -S make
+sudo pacman -S base-devel
+sudo pacman -S xorg-xinit
+sudo pacman -S vim
+sudo pacman -Syu xorg-server xorg-xinit
+sudo pacman -S ttf-martian-mono-nerd
+sudo pacman -S p7zip thunar-archive-plugin engrampa
+sudo pacman -S picom
+cd archive
+cd dwm
+sudo make clean install
+cd ..
+cd dwmblocks
+sudo make clean install
+cd ..
+cd dmenu
+sudo make clean install
+cd ..
+cd st
+sudo make clean install
+cd
+cd /tmp
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+sudo pacman -S zsh
+zsh
+0
+sudo pacman -S zsh-autosugestions zsh-syntax-highlighting
+vim ~/.xinitrc
