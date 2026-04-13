a2b9638d43df2d457effcace85e0c9887a9dd6ce
Invader788 <skorikandrew7@proton.me>
2026-04-13 16:50:55 -0400

    Add files via upload

---
+++ emacs/init.el
@@ -0,0 +1,30 @@
+;; ~/.emacs.d/init.el
+
+;; Bootstrap package system and use-package
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+
+(setq use-package-always-ensure t)   ; automatically install missing packages
+
+;; Load the actual configuration (keeps things organized)
+(org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
