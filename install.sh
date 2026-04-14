27d3538c64b994e0f9a9923cade1f8ccdc884472
Invader788 <skorikandrew7@proton.me>
2026-04-14 07:57:08 -0400

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+sudo pacman -S harfbuzz
+sudo pacman -S libxinerama
+sudo pacman -S libxft
+sudo pacman -S make
+sudo pacman -S base-devel
+sudo pacman -S xorg-xinit
+sudo pacman -S vim
+sudo pacman -Syu xorg-server xorg-xinit
+sudo pacman -S ttf-martian-mono-nerd
+sudo pacman -S p7zip thunar-archive-plugin engrampa
+sudo pacman -S picom
+sudo pacman -S zsh-autosuggestions zsh-syntax-highlighting
+cd archive
+cd dwm
+sudo make clean install
+cd ..
+cd dwmblocks
+sudo make clean install
+cd ..
+cd dmenu
+sudo make clean install
+cd ..
+cd st
+sudo make clean install
+cd
+cd /tmp
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+yay -S arc-gtk-theme
+mkdir ~/.xinitrc
+echo "#!/bin/sh
+export GTK_THEME="Arc-Dark"
+export XDG_CONFIG_DIRS="$XDG_CONFIG_DIRS"
+export GTK_APPLICATION_PREFERS_DARK_THEME=1
+dwmblocks &
+exec dwm" > /home/as/.xinitrc
+chmod +x ~/.xinitrc
+sudo pacman -S zsh
+zsh
+
