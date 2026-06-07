eec5b82e20a30c30a04da35583511e9600301085
Invader788 <skorikandrew7@proton.me>
2026-06-07 17:32:04 -0400

    Add files via upload

---
+++ FreeBSD/dmenu/config.mk
@@ -0,0 +1,48 @@
+# dmenu version
+VERSION = 5.4
+
+# paths
+PREFIX = /usr/local
+MANPREFIX = $(PREFIX)/share/man
+
+X11INC = /usr/X11R6/include
+X11LIB = /usr/X11R6/lib
+
+# Xinerama, comment if you don't want it
+XINERAMALIBS  = -lXinerama
+XINERAMAFLAGS = -DXINERAMA
+
+# freetype
+FREETYPELIBS = -lfontconfig -lXft
+FREETYPEINC = /usr/include/freetype2
+
+# includes and libs
+INCS = -I$(X11INC) -I$(FREETYPEINC)
+LIBS = -L$(X11LIB) -lX11 $(XINERAMALIBS) $(FREETYPELIBS)
+
+# flags
+CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700 -D_POSIX_C_SOURCE=200809L -DVERSION=\"$(VERSION)\" $(XINERAMAFLAGS)
+CFLAGS   = -std=c99 -pedantic -Wall -Os $(INCS) $(CPPFLAGS)
+LDFLAGS  = $(LIBS)
+
+# compiler and linker
+CC = cc
+
+# OS-specific configuration
+UNAME := $(shell uname -s)
+
+# FreeBSD
+ifeq ($(UNAME), FreeBSD)
+X11INC = /usr/local/include
+X11LIB = /usr/local/lib
+FREETYPEINC = /usr/local/include/freetype2
+LIBS = -L$(X11LIB) -lX11 $(XINERAMALIBS) $(FREETYPELIBS)
+CPPFLAGS = -D_DEFAULT_SOURCE -D_XOPEN_SOURCE=700 -D_POSIX_C_SOURCE=200809L -DVERSION=\"$(VERSION)\" $(XINERAMAFLAGS)
+MANPREFIX = $(PREFIX)/man
+endif
+
+# OpenBSD
+ifeq ($(UNAME), OpenBSD)
+FREETYPEINC = $(X11INC)/freetype2
+MANPREFIX = $(PREFIX)/man
+endif
