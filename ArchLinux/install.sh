72c15b91a2c72e33501942a2c192f0c7d6780cd4
Invader788 <skorikandrew7@proton.me>
2026-09-06 14:32:26 -0400

    Add files via upload

---
+++ ArchLinux/install.sh
@@ -0,0 +1,21 @@
+sudo pacman -Syu --noconfirm
+
+sudo pacman -S --needed --noconfirm \
+  base-devel \
+  git \
+  harfbuzz \
+  libxinerama \
+  libxft \
+  xorg-server \
+  xorg-xinit \
+  neovim \
+  zsh \
+  zsh-autosuggestions \
+  zsh-syntax-highlighting \
+  feh \
+  picom \
+  thunar \
+  thunar-archive-plugin \
+  engrampa \
+  p7zip \
+  ttf-martian-mono-nerd
